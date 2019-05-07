47e8e23d76acb51c7a2933b347b836cb342d42fb
Sergii Safatiuk <s.safatiuk@gmail.com>
2019-05-07 23:35:07 +0200

    Installing Autofac and config and injecting dependency properties for MainWindow.

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -1,5 +1,5 @@
-﻿using FriendOrganizer.UI.Data;
-using FriendOrganizer.UI.ViewModel;
+﻿using Autofac;
+using FriendOrganizer.UI.Startup;
 using System.Windows;
 
 namespace FriendOrganizer.UI
@@ -11,8 +11,10 @@ namespace FriendOrganizer.UI
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            var mainWindow = new MainWindow(
-                new MainViewModel(new FriendDataService()));
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
 
             mainWindow.Show();
         }
---
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Startup
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<FriendDataService>().As<IDataService<Friend>>();
+
+            return builder.Build();
+        }
+    }
+}
