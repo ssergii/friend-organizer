bc056b5f4f874e99ae010899b8a4c688ae61bc69
Sergii Safatiuk <s.safatiuk@gmail.com>
2019-05-10 21:40:56 +0200

    Publish the OpenFriendDetailViewEvent.

--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -1,22 +1,48 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : BaseViewModel, INavigationViewModel
     {
+        #region fields and properties
         private IFriendLookupDataService _dataService;
+        private IEventAggregator _eventAggregator;
 
-        public NavigationViewModel(IFriendLookupDataService dataService)
+        private LookupItem _selecetedFriend;
+        public LookupItem SelecetedFriend
         {
-            _dataService = dataService;
-            Friends = new ObservableCollection<LookupItem>();
+            get { return _selecetedFriend; }
+            set
+            {
+                _selecetedFriend = value;
+                OnPropertyChanged();
+
+                if (_selecetedFriend != null)
+                    _eventAggregator
+                        .GetEvent<OpenFriendDetailViewEvent>()
+                        .Publish(_selecetedFriend.Id);
+            }
         }
 
         public ObservableCollection<LookupItem> Friends { get; }
+        #endregion
+
+        public NavigationViewModel(
+            IFriendLookupDataService dataService,
+            IEventAggregator eventAggregator)
+        {
+            _dataService = dataService;
+            _eventAggregator = eventAggregator;
+
+            Friends = new ObservableCollection<LookupItem>();
+        }
+
 
         public async Task LoadAsync()
         {
