23924d4e5028cfeca7ef19fbec0b1a0b6f4b672d
Sergii Safatiuk <s.safatiuk@gmail.com>
2019-05-11 20:28:31 +0200

    Implement INotifyDataErrorInfo.

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,15 +1,21 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.ViewModel;
+using System.ComponentModel;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper : BaseViewModel
+    public class FriendWrapper : BaseViewModel, INotifyDataErrorInfo
     {
         public FriendWrapper(Friend model)
         {
             Model = model;
         }
 
+        #region model wrapping
         public Friend Model { get; }
 
         public int Id { get; }
@@ -43,5 +49,50 @@ namespace FriendOrganizer.UI.Wrapper
                 OnPropertyChanged();
             }
         }
+        #endregion
+
+        #region INotifyDataErrorInfo members implementations
+        private Dictionary<string, List<string>> _errorsByProperyName
+          = new Dictionary<string, List<string>>();
+
+        public bool HasErrors
+        {
+            get { return _errorsByProperyName.Any(); }
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorsByProperyName.ContainsKey(propertyName) ?
+                _errorsByProperyName[propertyName] : null;
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void AddError(string propertyName, string error)
+        {
+            if (!_errorsByProperyName.ContainsKey(propertyName))
+                _errorsByProperyName[propertyName] = new List<string>();
+
+            if (!_errorsByProperyName[propertyName].Contains(error))
+            {
+                _errorsByProperyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        private void RemoveError(string propertyName)
+        {
+            if (_errorsByProperyName.ContainsKey(propertyName))
+            {
+                _errorsByProperyName.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+        #endregion
     }
 }
