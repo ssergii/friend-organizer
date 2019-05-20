1687df9e4ee640dbf56a4e81c3d4e84fc4e6c93f
Sergii Safatiuk <s.safatiuk@gmail.com>
2019-05-20 22:28:44 +0200

    Wrap the Meeting Entity.

---
+++ FriendOrganizer.UI/Wrapper/MeetingWrapper.cs
@@ -0,0 +1,39 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class MeetingWrapper : ModelWrapper<Meeting>
+    {
+        public MeetingWrapper(Meeting model) : base(model) { }
+
+        #region properties
+        public int Id
+        {
+            get { return GetValue<int>(); }
+        }
+
+        public string Title
+        {
+            get { return GetValue<string>();  }
+            set { SetValue(value); }
+        }
+
+        public DateTime DateFrom
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+
+        public DateTime DateTo
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+        #endregion
+    }
+}
