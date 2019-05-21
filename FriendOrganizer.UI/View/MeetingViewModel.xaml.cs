017ba68f7a9eee990c3eb0ec4a81056555991155
Sergii Safatiuk <s.safatiuk@gmail.com>
2019-05-21 23:50:33 +0200

    Create the MeetingDetailView.

---
+++ FriendOrganizer.UI/View/MeetingViewModel.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FriendOrganizer.UI.View
+{
+    public partial class MeetingViewModel : UserControl
+    {
+        public MeetingViewModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
